69ea8380c9da3fd2af9cd026219cd63a0a037be8
yuan <wangpajeon@gmail.com>
2024-03-24 03:29:04 +0900

    initial commit

---
+++ js/모르겟다.js
@@ -0,0 +1,32 @@
+
+/*어떻게 하는지 잘 모르겠어서..복붙했습니다...*/
+function includeHTML(callback) {
+    var z, i, elmnt, file, xhr;
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        file = elmnt.getAttribute("include-html");
+
+        if (file) {
+            xhr = new XMLHttpRequest();
+            xhr.onreadystatechange = function() {
+                if (this.readyState == 4) {
+                    if (this.status == 200) {
+                        elmnt.innerHTML = this.responseText;
+                    }
+                    if (this.status == 404) {
+                        elmnt.innerHTML = "Page not found.";
+                    }
+                    elmnt.removeAttribute("include-html");
+                    includeHTML(callback);
+                }
+            };
+            xhr.open("GET", file, true);
+            xhr.send();
+            return;
+        }
+    }
+    setTimeout(function() {
+        callback();
+    }, 0);
+}
